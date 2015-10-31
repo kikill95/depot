c51784dabad7fd447172d4180060ff4de64a6789
kikill95 <nokia6030>
2015-10-31 18:24:15 +0200

    Add validations and fixes for views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ActiveRecord::Base
   has_secure_password
 
-  validates :email, presence: true
+  validates :email, :password, presence: true
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+  validates :email, uniqueness: true
+  validates :password, length: { in: 6..20 }
+  validates :password_confirmation, length: { in: 6..20 }
 end

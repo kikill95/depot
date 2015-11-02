b9696b3dfc3cf132e70ebe8f4ba1aac6631657db
kikill95 <nokia6030>
2015-11-03 00:03:29 +0200

    Edit user_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,10 +6,5 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params.require(:user).permit!)
     render :new unless @user.save
-    # if @user.save
-    #   redirect_to root_path
-    # else
-    #   render :new
-    # end
   end
 end

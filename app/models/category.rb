1fd4ae9615cf272c093b1394ec930780fe0cdc4b
kikill95 <kirill.gus@gmail.com>
2015-11-28 12:46:44 +0200

    Friendly category url(categories/name)

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,14 @@
 class Category < ActiveRecord::Base
   has_many :products
   validates :title, presence: true, uniqueness: true
+
+  before_create :create_slug
+  
+  def to_param
+    slug
+  end
+    
+  def create_slug
+    self.slug = self.title.parameterize
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resource :user
   resource :session, only: [:new, :create, :destroy]
   root 'welcome#index'
+  match 'categories/:slug' => 'welcome#index', via: :get, as: 'categories'
   resources :users do
     get :confirm, on: :member
   end

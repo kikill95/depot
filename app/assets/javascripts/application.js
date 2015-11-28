9cf1d208607d8e3fedc1cccb46c1169839d8cf18
kikill95 <kirill.gus@gmail.com>
2015-11-28 23:10:08 +0200

    Show product bootsrap popup with detaiks

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -30,3 +30,7 @@ $(document).on('change', 'input#product_image', function(){
 $(document).on('keyup', '#_search', function(){
   $(this.form).trigger('submit.rails');
 })
+
+$(function () {
+  $('[data-toggle="popover"]').popover();
+})

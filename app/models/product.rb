02a195a3639c95ca09a63fa6fec4d8bc7f71a72b
kikill95 <kirill.gus@gmail.com>
2015-11-30 22:31:18 +0200

    Fixes after teacher's review: remove condition for categories route

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ActiveRecord::Base
   belongs_to :category
   validates :name, presence: true, uniqueness: true
   validates :price, presence: true
-  scope :by_category, -> (slug) { where(category: Category.find_by_slug(slug)) if slug }
+  scope :by_category, -> (slug) { joins(:category).where('categories.slug = ?', slug) if slug }
   scope :search, -> (name) { where('name ILIKE :q OR description ILIKE :q', q: "%#{name}%") if name }
   scope :awesome, -> (params) { search(params[:search]).by_category(params[:slug]).page(params[:page]).per(8) }
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resource :user
   resource :session, only: [:new, :create, :destroy]
   root 'welcome#index'
-  match 'categories/:slug' => 'welcome#index', via: :get, as: 'categories'
+  get 'categories/(:slug)', to: 'welcome#index', as: :categories
   resources :users do
     get :confirm, on: :member
   end
